d77e5270e6082a7b516542c70bcf7f46e6999f9e
Anthony Todd <toddy201@gmail.com>
2014-03-31 19:20:22 +0100

    created DNA component with editor

--- Assets/Scripts/DNA.cs
+++ Assets/Scripts/DNA.cs
@@ -2,72 +2,73 @@
 using System;
 using System.Collections;
 
-public enum GATC
-{
-    Red, Blue, Yellow, Green
-}
-
-[Serializable]
-public struct SDNA
-{
-    private GATC[] _top;
-    private GATC[] _bottom;
-
-    private int _length;
-
-    public GATC[] top
-    {
-        get
-        {
-            return _top;
-        }
-    }
-
-    public GATC[] bottom
-    {
-        get
-        {
-            return _bottom;
-        }
-    }
-
-    public int length
-    {
-        get
-        {
-            return _length;
-        }
-
-        set
-        {
-            _length = value;
-            if (_length == 0)
-            {
-                _top = null;
-                _bottom = null;
-            }
-            else
-            {
-                Array.Resize<GATC>(ref _top, _length);
-                Array.Resize<GATC>(ref _bottom, _length);
-            }
-        }
-    }
-
-    public SDNA(int length)
-    {
-        _top = null;
-        _bottom = null;
-        _length = 0;
-
-        this.length = length;
-    }
-
-}
-
 public class DNA : MonoBehaviour {
 
-    public SDNA dna = new SDNA();
+    public enum GATC
+    {
+        Red, Blue, Yellow, Green
+    }
+
+    [Serializable]
+    public class DNA_Data
+    {
+        private GATC[] _top;
+        private GATC[] _bottom;
+
+        private int _length;
+
+        public GATC[] top
+        {
+            get
+            {
+                return _top;
+            }
+        }
+
+        public GATC[] bottom
+        {
+            get
+            {
+                return _bottom;
+            }
+        }
+
+        public int length
+        {
+            get
+            {
+                return _length;
+            }
+
+            set
+            {
+                _length = value;
+                if (_length == 0)
+                {
+                    _top = null;
+                    _bottom = null;
+                }
+                else
+                {
+                    Array.Resize<GATC>(ref _top, _length);
+                    Array.Resize<GATC>(ref _bottom, _length);
+                }
+            }
+        }
+
+        public DNA_Data(int length)
+        {
+            _top = null;
+            _bottom = null;
+            _length = 0;
+
+            this.length = length;
+        }
+
+    }
+
+
+    public DNA_Data dna = new DNA_Data(0);
 	// Use this for initialization
 	void Start () {
 	
---
+++ Assets/Scripts/Editor/DNA_Editor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(DNA))]
+public class DNA_Editor : Editor {
+
+    Vector2 scroll = new Vector2(0, 0);
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        DNA myTarget = (DNA)target;
+
+        scroll = EditorGUILayout.BeginScrollView(scroll, GUILayout.Height(100f));
+
+        myTarget.dna.length = Mathf.Max(0, EditorGUILayout.IntField("Length", myTarget.dna.length));
+
+        EditorGUILayout.BeginHorizontal();
+        for (int i = 0; i < myTarget.dna.length; i++)
+        {
+            EditorGUILayout.BeginVertical();
+            EditorGUILayout.LabelField((i+1).ToString(), GUILayout.Width(32.0f));
+            myTarget.dna.top[i] = (DNA.GATC)EditorGUILayout.EnumPopup(myTarget.dna.top[i]);
+            myTarget.dna.bottom[i] = (DNA.GATC)EditorGUILayout.EnumPopup(myTarget.dna.bottom[i]);
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndScrollView();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
